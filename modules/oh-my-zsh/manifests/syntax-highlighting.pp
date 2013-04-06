9ee0db844765df283601aca9cfad52c92324cdf2
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-06 01:23:47 -0400

    fix custom zsh modules

--- modules/oh-my-zsh/manifests/syntax-highlighting.pp
+++ modules/oh-my-zsh/manifests/syntax-highlighting.pp
@@ -1,4 +1,4 @@
-class oh-my-zsh-syntax-highlighting {
+class oh-my-zsh::syntax-highlighting {
   repository {'zsh-users_zsh-syntax-highlighting':
     source => 'zsh-users/zsh-syntax-highlighting',
     path   => "/Users/${::luser}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting",
