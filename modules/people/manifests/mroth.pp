c1e0c4c40c1ed0cb431afdfbffa8af5551c2641a
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-05 21:54:11 -0400

    WIP of personal manifest

---
+++ modules/people/manifests/mroth.pp
@@ -0,0 +1,63 @@
+#some of the things I include here *might* be in the default site manifest,
+#but to make this more portable I shall include them anyhow despite redundancy.
+#in general, I'm going to list everything I consider an "application"... kinda
+class people::mroth {
+
+  #include ALL the browsers
+  include chrome::stable
+  include firefox
+
+  #dev-y stuff that i personally want even outside of projects
+  include redis
+  include heroku
+
+  #text editors i hate the least
+  include macvim
+  include sublime_text_2
+  include mou
+  #TODO: janus for macvim
+
+  #terminalz
+  include zsh
+  include oh-my-zsh-syntax              #custom module
+  include oh-my-zsh-syntax highlighting #custom module
+  include iterm2::dev
+  #TODO: set default color scheme / prefs for iterm2
+
+  #virtual machines
+  include vmware-fusion
+  include virtualbox #because we haven't forked over $ for the vagrant vmware plugin yet
+  include vagrant
+
+  #non-dev stuff for general productivity
+  include hipchat
+  include dropbox
+  include caffeine
+  include sizeup
+  include vlc
+  include flux
+
+  #lolcommits is gunna want the below
+  include xquartz
+  include imagemagick
+
+  # some sensible OSX defaults
+  # can eventually replace with https://github.com/boxen/puppet-osx/pull/3 ?
+  boxen::osx_defaults { "Disable 'natural scrolling'":
+    key    => 'com.apple.swipescrolldirection',
+    domain => 'NSGlobalDomain',
+    value  => 'false',
+  }
+  #TODO: no more aqua colors
+
+  # more homebrew packages I personally want
+  package {
+    [
+      'imagesnap', #webcams are meant to be CLI tools
+      'pianobar'  #music is meant to be listened to from CLI
+    ]:
+  }
+
+  #TODO: dotfiles!
+
+}
