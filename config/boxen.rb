dc7716295a3b73309f5622bfa20d445ead529303
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-22 16:49:46 -0400

    ok thats actually annoying when its just me

--- config/boxen.rb
+++ config/boxen.rb
@@ -13,7 +13,7 @@ ENV['BOXEN_REPO_NAME'] = 'mroth/my-boxen'
 # ENV["BOXEN_S3_BUCKET"] = ''
 
 # Auto-report issues on failed runs
-ENV["BOXEN_ISSUES_ENABLED"] = 'yes'
+# ENV["BOXEN_ISSUES_ENABLED"] = 'yes'
 
 # Submit audit data to an arbitrary HTTP endpoint
 # ENV["BOXEN_WEB_HOOK_URL"] = 'https://some-uri.com/boxen'
