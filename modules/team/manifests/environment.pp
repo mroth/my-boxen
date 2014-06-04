b725c9dbd53dd52cecb45ac6c7743b77f4b0034e
Matthew Rothenberg <mrothenberg@gmail.com>
2014-06-04 17:36:58 -0400

    bump ruby to 8.0x series
    lots of changes here, introducing some hiera configs to preserve using
    rbenv instead of chruby (no reason to change now and it doesnt appear to
    support automatically moving existing installs so...)
    
    also manually bumping ruby-build in hiera so we can install 2.1.2
    cleanly.

--- modules/team/manifests/environment.pp
+++ modules/team/manifests/environment.pp
@@ -18,9 +18,14 @@ class team::environment {
   ruby::version { '1.8.7': }
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.1.1': }
+  ruby::version { '2.1.2': }
   class { 'ruby::global':
-    version => '2.1.1'
+    version => '2.1.2'
+  }
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.0',
+    ruby_version => '*',
   }
 
   # let's make sure everyone has nodejs and coffeescript as a minimum
